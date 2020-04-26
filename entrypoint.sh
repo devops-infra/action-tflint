15cd1d662ec8b3bc252156a090e414dedf2b0e95
Krzysztof Szyper <krzysztof.szyper@gmail.com>
2020-04-26 22:00:40 +0200

    Run terraform init before tflint

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,9 +16,11 @@ for PREFIX in "${ARRAY[@]}"; do
         cd "${WORK_DIR}/${DIRECTORY}" || RET_CODE=1
         echo -e "\nDirectory: ${DIRECTORY}"
         if [[ -f "${WORK_DIR}/${INPUT_TFLINT_CONFIG}" ]]; then
-            tflint -c "${WORK_DIR}/${INPUT_TFLINT_CONFIG}" || RET_CODE=1
+          terraform init
+          tflint -c "${WORK_DIR}/${INPUT_TFLINT_CONFIG}" || RET_CODE=1
         else
-            tflint "${INPUT_TFLINT_PARAMS}" || RET_CODE=1
+          terraform init
+          tflint "${INPUT_TFLINT_PARAMS}" || RET_CODE=1
         fi
         cd ${WORK_DIR} || RET_CODE=1
     done
